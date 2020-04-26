9105c7a05df8e4d17a39054caff1efe4d13d6b6d
Chloe Chanson <chloe.cloclo8@live.fr>
2020-04-26 18:59:40 +0200

    troisime changement

--- index.php
+++ index.php
@@ -16,3 +16,4 @@ define( 'WP_USE_THEMES', true );
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
 ///petit changement qui sert à rien
+//troisième changement qui sert à rien
